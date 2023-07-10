45fd94cac88ebd9b47d2b39f0649dd2abd01a081
Alex Cho <38528052+alexcho617@users.noreply.github.com>
2023-07-10 16:56:42 +0900

    [SetUp] Project Initialized
    - 협업을 위한 프로젝트를 생성하였습니다.
    - pages 폴더 안에 각 화면 별로 각각의 dart파일을 생성했습니다.
    - 자신이 맡은 화면 외에 작업이나 수정이 필요한 경우 해당 화면 담당하신 분께 꼭 먼저 여쭤보세요 :)

---
+++ hello_i1e4/lib/pages/listPage.dart
@@ -0,0 +1,16 @@
+//리스트화면
+
+import 'package:flutter/material.dart';
+
+class ListPage extends StatelessWidget {
+  ListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('LIST Page'),
+      ),
+    );
+  }
+}
