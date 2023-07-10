b578f3c90ebcbc9434eb7c86247de9086621c4f5
Alex Cho <38528052+alexcho617@users.noreply.github.com>
2023-07-10 17:03:56 +0900

    attempt direct main push

--- hello_i1e4/lib/pages/listPage.dart
+++ hello_i1e4/lib/pages/listPage.dart
@@ -1,5 +1,4 @@
-//리스트화면
-
+// ignore_for_file: prefer_const_constructors_in_immutables, prefer_const_constructors
 import 'package:flutter/material.dart';
 
 class ListPage extends StatelessWidget {
@@ -8,6 +7,7 @@ class ListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: Text("E1A4")),
       body: Center(
         child: Text('LIST Page'),
       ),
