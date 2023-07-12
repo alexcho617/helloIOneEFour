dfb7b8c0c706cce52ec421147958bfd9b71bf36e
jiwon <erica1021@naver.com>
2023-07-12 19:39:16 +0900

    re: update members from Jiwon

--- hello_i1e4/lib/service/member_service.dart
+++ hello_i1e4/lib/service/member_service.dart
@@ -10,62 +10,66 @@ class MemberService extends ChangeNotifier {
   }
 
   List<TeamMember> teamList = [
-      TeamMember(
-        name: "홍길동",
-        mbti: "ISTJ",
-        city: "서울",
-        comment: "커피를 마시면 에너지가 넘칩니다.",
-        pic: "",
-      ),
-      TeamMember(
-        name: "김영희",
-        mbti: "ENFP",
-        city: "부산",
-        comment: "노래를 잘 부릅니다.",
-        pic: "",
-      ),
-      TeamMember(
-        name: "박철수",
-        mbti: "ESTJ",
-        city: "대구",
-        comment: "요리를 하는 것을 즐깁니다.",
-        pic: "",
-      ),
-      TeamMember(
-        name: "이미영",
-        mbti: "INTP",
-        city: "인천",
-        comment: "여행을 좋아합니다.",
-        pic: "",
-      ),
-      TeamMember(
-        name: "최민식",
-        mbti: "ISFP",
-        city: "대전",
-        comment: "악기 연주를 잘합니다.",
-        pic: "",
-      ),
-    ];
+    TeamMember(
+      name: "홍길동",
+      mbti: "ISTJ",
+      city: "서울",
+      comment: "커피를 마시면 에너지가 넘칩니다.",
+      pic: "",
+    ),
+    TeamMember(
+      name: "김영희",
+      mbti: "ENFP",
+      city: "부산",
+      comment: "노래를 잘 부릅니다.",
+      pic: "",
+    ),
+    TeamMember(
+      name: "박철수",
+      mbti: "ESTJ",
+      city: "대구",
+      comment: "요리를 하는 것을 즐깁니다.",
+      pic: "",
+    ),
+    TeamMember(
+      name: "이미영",
+      mbti: "INTP",
+      city: "인천",
+      comment: "여행을 좋아합니다.",
+      pic: "",
+    ),
+    TeamMember(
+      name: "최민식",
+      mbti: "ISFP",
+      city: "대전",
+      comment: "악기 연주를 잘합니다.",
+      pic: "",
+    ),
+  ];
 
-// save
-  saveMember() {
+  // Save members
+  void saveMembers() {
     List memberJson = teamList.map((member) => member.toJson()).toList();
     String jsonString = jsonEncode(memberJson);
     preferences.setString('memberList', jsonString);
   }
 
-// load
-  loadMember() {
+  // Load members
+  void loadMembers() {
     String? jsonString = preferences.getString('memberList');
     if (jsonString == null) return;
-    List memeberJson = jsonDecode(jsonString);
-    teamList = memeberJson.map((json) => TeamMember.fromJson(json)).toList();
+    List memberJson = jsonDecode(jsonString);
+    teamList = memberJson.map((json) => TeamMember.fromJson(json)).toList();
   }
 
-// create member
-
-// delete member
-
-// update member
-
+  // Update members
+  void updateMember(TeamMember updatedMember) {
+    int index =
+        teamList.indexWhere((member) => member.name == updatedMember.name);
+    if (index != -1) {
+      teamList[index] = updatedMember;
+      saveMembers(); // Save the updated list
+      notifyListeners(); // Notify listeners of the change
+    }
+  }
 }
